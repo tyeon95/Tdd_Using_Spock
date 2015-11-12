16f6d95970f44e792ff8f073f599022c1b37044f
Roof Panichsombat <roofimon@Roofs-MacBook-Air.local>
2015-11-12 20:56:52 +0700

    Refactor test to data driven

--- src/test/groovy/com/mrhaki/blog/CaptchaTest.groovy
+++ src/test/groovy/com/mrhaki/blog/CaptchaTest.groovy
@@ -3,18 +3,31 @@ package com.mrhaki.blog
 import spock.lang.Specification
 
 class CaptchaTest extends Specification{
-    def "First pattern left operand should be One"() {
-        def captcha = new Captcha(1, 1, 1, 1)
-        expect: captcha.left() == "One"
+    def "First pattern left operand shold be numeric string"() {
+        setup:
+            def captcha = new Captcha(pattern, left, operator, right)
+
+        expect:
+            captcha.left() == result
+
+        where:
+            pattern | left | operator | right | result
+            1       | 1    | 1        | 1     | "One"
+            1       | 2    | 1        | 1     | "Two"
+            1       | 9    | 1        | 1     | "Nine"
     }
 
-    def "First pattern left operand should be Nine"() {
-        def captcha = new Captcha(1, 9, 1, 1)
-        expect: captcha.left() == "Nine"
-    }
+    def "Second pattern left operand shold be numeric"() {
+        setup:
+            def captcha = new Captcha(pattern, left, operator, right)
 
-    def "Second pattern left operand should be 1"() {
-        def captcha = new Captcha(2, 1, 1, 1)
-        expect: captcha.left() == "1"
+        expect:
+            captcha.left() == result
+
+        where:
+            pattern | left | operator | right | result
+            2       | 1    | 1        | 1     | "1"
+            2       | 2    | 1        | 1     | "2"
+            2       | 9    | 1        | 1     | "9"
     }
 }
